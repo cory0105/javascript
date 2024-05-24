cc606e7a395d2fdef6175707fda43df834477e84
cory0105 <161662652+cory0105@users.noreply.github.com>
2024-05-24 17:14:41 +0900

    0524

---
+++ 0524/script.js
@@ -0,0 +1,15 @@
+// const container = document.querySelector(".container");
+// console.log(container);
+// const divArray = document.querySelectorAll("div");
+// divArray.forEach((div)=>console.log(div));
+
+const cardArray = document.querySelectorAll(".namecard");
+cardArray.forEach((card)=>{
+    card.addEventListener("click", ()=>{
+        console.log(card);
+    })
+})
+// 좋지 않은 코드
+// cardArray[0].addEventListener("click",()=>{
+//     console.log(cardArray[0]);
+// })
